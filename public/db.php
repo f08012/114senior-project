e72992d822f9783a0904d1863f686f17a0c25a3f
f08012 <f11108012@gtrial.ctust.edu.tw>
2025-05-05 17:36:23 +0800

    Update db.php

--- public/db.php
+++ public/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // 如果有設定密碼，就填進來
-$dbname = "abc"; // ← 這裡就是資料庫名稱
+$servername = "d0c7lphr0fns73e50730-a.oregon-postgres.render.com";
+$username = "reservations_sql_user";
+$password = "WBC5ij6U1fp7c6Q1DR8B4VrCNyDdD2OR"; // 如果有設定密碼，就填進來
+$dbname = "reservations_sql"; // ← 這裡就是資料庫名稱
 
 // 建立連線
 $conn = new mysqli($servername, $username, $password, $dbname);
